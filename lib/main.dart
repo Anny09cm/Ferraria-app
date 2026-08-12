519eb94d7250dc6974c4bf9b7460a738cbc5dd21
Anny09cm <anaidcm9@gmail.com>
2026-08-12 03:05:40 -0600

    Firebase vinculado y configurado

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:ferraria/screens/login_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const AppFerraria());
 }
 
